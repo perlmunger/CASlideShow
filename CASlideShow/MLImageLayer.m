fc45e11cb9a67e76ead7f05932a246f3f4f96288
Alex Gray <alex@mrgray.com>
2013-10-28 14:14:14 -0400

    refactored the project to better illustrate / make more clear it's point.

--- CASlideShow/MLImageLayer.m
+++ CASlideShow/MLImageLayer.m
@@ -10,29 +10,25 @@
 
 @implementation MLImageLayer
 
-- (id)initWithLayer:(id)layer
-{
-	if(self = [super initWithLayer:layer]) {
-		if([layer isKindOfClass:[MLImageLayer class]]) {
-			MLImageLayer *other = (MLImageLayer*)layer;
-      [self setCounter:[other counter]];
-		}
-	}
-	return self;
-}
+- (id)initWithLayer:(id)layer	{	return self = [super initWithLayer:layer] ?
 
-+ (BOOL)needsDisplayForKey:(NSString*)key
-{
-  if ([key isEqualToString:@"counter"]) {
-    return YES;
-  } else {
-    return [super needsDisplayForKey:key];
-  }
+	 [layer isKindOfClass:MLImageLayer.class] ? [self setCounter:[(MLImageLayer*)layer counter]] : nil,
+																			 self : nil;
 }
++ (BOOL)needsDisplayForKey:(NSString*)key {
 
-- (void)drawInContext:(CGContextRef)ctx
-{
-  DLog(@"Counter is: %d", _counter);
+	return [key isEqualToString:@"counter"] ?: [super needsDisplayForKey:key];
+}
+- (void)drawInContext:(CGContextRef)ctx		{ NSString *ctr = [NSString stringWithFormat:@"Counter is: %d", _counter]; DLog(@"%@",ctr);
+
+    UIGraphicsPushContext(ctx); CGRect rect;
+
+		[[UIImage imageWithCGImage:self.image] drawInRect:rect = CGRectInset(self.bounds, 100, 100)]; rect = CGRectOffset(rect, 30, 30);
+		[[NSAttributedString.alloc initWithString:ctr
+																	 attributes:@{NSForegroundColorAttributeName:UIColor.redColor,
+																								NSFontAttributeName:[UIFont fontWithName:@"AmericanTypewriter" size:48]}]
+																  drawAtPoint:rect.origin];
+		UIGraphicsPopContext();
 }
 
 @end
--- CASlideShow/MLViewController.m
+++ CASlideShow/MLViewController.m
@@ -7,52 +7,37 @@
 //
 
 #import "MLViewController.h"
-#import "MLImageLayer.h"
 
-@interface MLViewController ()
-
+@interface MLViewController () {	CAKeyframeAnimation *slideShowAnimation, *counterAnimation;	CAAnimationGroup *group;}
 @end
 
 @implementation MLViewController
 
-- (void)viewDidLoad
-{
-  [super viewDidLoad];
+- (void)viewDidLoad	{	[super viewDidLoad];
 
-  _animationLayer = [MLImageLayer layer];
-  [_animationLayer setBounds:CGRectMake(0.0f, 0.0f, 400.0f, 320.0f)];
-  [_animationLayer setPosition:[[self view] center]];
-  
-  UIImage *image = [UIImage imageNamed:@"Countryside.jpg"];
-  [_animationLayer setContents:(__bridge id)[image CGImage]];
-  
-  [[[self view] layer] addSublayer:_animationLayer];
-  
-  CAKeyframeAnimation *slideShowAnimation = [CAKeyframeAnimation animationWithKeyPath:@"contents"];
-  [slideShowAnimation setValues:@[(id)[[UIImage imageNamed:@"Countryside.jpg"] CGImage],
-   (id)[[UIImage imageNamed:@"Countryside-1.jpg"] CGImage],
-   (id)[[UIImage imageNamed:@"Countryside-2.jpg"] CGImage],
-   (id)[[UIImage imageNamed:@"Countryside-3.jpg"] CGImage],
-   (id)[[UIImage imageNamed:@"Countryside-4.jpg"] CGImage],
-   (id)[[UIImage imageNamed:@"Countryside-5.jpg"] CGImage]]];
-  
-  [slideShowAnimation setDuration:5.0];
-  [slideShowAnimation setDelegate:self];
-  [slideShowAnimation setRepeatCount:HUGE_VALF];
-  [slideShowAnimation setCalculationMode:kCAAnimationDiscrete];
-  
-  CAKeyframeAnimation *counterAnimation = [CAKeyframeAnimation animationWithKeyPath:@"counter"];
-  [counterAnimation setDelegate:self];
-  [counterAnimation setValues:@[@(0), @(1), @(2), @(3), @(4), @(5)]];
-  [counterAnimation setDuration:5.0];
-  [counterAnimation setRepeatCount:HUGE_VALF];
-  [counterAnimation setCalculationMode:kCAAnimationDiscrete];
-  
-  CAAnimationGroup *group = [CAAnimationGroup animation];
-  [group setDuration:5.0];
-  [group setRepeatCount:HUGE_VALF];
-  [group setAnimations:@[slideShowAnimation, counterAnimation]];
-  
+	self.view.layer.sublayers						= @[_animationLayer = MLImageLayer.new];
+	_animationLayer.frame								= self.view.bounds;
+
+	group																= CAAnimationGroup.animation;
+	group.animations = @[
+		slideShowAnimation								= [CAKeyframeAnimation animationWithKeyPath:@"image"],
+		counterAnimation									= [CAKeyframeAnimation animationWithKeyPath:@"counter"]
+	];
+	slideShowAnimation.duration					= counterAnimation.duration					= group.duration		= 10.0;
+	slideShowAnimation.repeatCount			= counterAnimation.repeatCount			= group.repeatCount = HUGE_VALF;
+
+	_animationLayer.image  = [UIImage imageNamed:@"Countryside.jpg"  ].CGImage;
+
+  counterAnimation.values							= @[@0, @1, @2, @3, @4, @5];
+	slideShowAnimation.values						= @[
+
+		(id)[UIImage imageNamed:@"Countryside.jpg"  ].CGImage,
+		(id)[UIImage imageNamed:@"Countryside-1.jpg"].CGImage,
+		(id)[UIImage imageNamed:@"Countryside-2.jpg"].CGImage,
+		(id)[UIImage imageNamed:@"Countryside-3.jpg"].CGImage,
+		(id)[UIImage imageNamed:@"Countryside-4.jpg"].CGImage,
+		(id)[UIImage imageNamed:@"Countryside-5.jpg"].CGImage ];
+
   [_animationLayer addAnimation:group forKey:nil];
 }
 
